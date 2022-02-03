e83329ffda99779ef49d41a15f67fea42a3e3c74
William <williambecker321@gmail.com>
2022-02-03 12:06:42 +0100

    Now display the text

--- generator.js
+++ generator.js
@@ -1,5 +1,32 @@
 'use strict';
 
-const body = document.getElementsByTagName('body');
+const mainBody = document.getElementById('body');
 
-fetch('/mall.txt').then(x => x.text()).then(text => console.log(text));
+let textSplit
+
+const generator = (text) => {
+    textSplit = text.split('\r\n')
+    console.log(textSplit);
+    let kap = document.createElement('div');
+    textSplit.forEach((stycke, i) => {
+        if (i == 0) {
+            let h2 = document.createElement('h2');
+            h2.textContent = stycke;
+            kap.append(h2)
+        }
+        else if (stycke === ''){}
+        else {
+            const p = document.createElement('p');
+            p.textContent = stycke;
+            kap.append(p);
+        }
+        
+    });
+
+    mainBody.append(kap);
+}
+
+
+
+
+fetch('/mall.txt').then(x => x.text()).then(text => generator(text));
